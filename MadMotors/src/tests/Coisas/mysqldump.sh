249a8e3f023b8d1d2209b901665b9c4e74c97ba9
cristovaoolegario <cristovaoolegario@gmail.com>
2017-05-28 00:49:34 -0300

    Adição de um comentário explicativo sobre a função 'mysqldump'

--- MadMotors/src/tests/Coisas/mysqldump.sh
+++ MadMotors/src/tests/Coisas/mysqldump.sh
@@ -1,1 +1,2 @@
- mysqldump --xml -t -h pucsi.mysql.database.azure.com -u pucsi@pucsi --password ti_testes > ti_testes.xml
+//Gera o xlm do banco para que os testes possam ser feitos com o phpUnit
+mysqldump --xml -t -h pucsi.mysql.database.azure.com -u pucsi@pucsi --password ti_testes > ti_testes.xml
