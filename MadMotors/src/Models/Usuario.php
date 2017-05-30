2e002902dcd7d5afd45b6deeb8d284752cc40e03
cristovaoolegario <cristovaoolegario@gmail.com>
2017-05-30 20:56:04 -0300

    Mudança dos métodos da classe Usuario para de instância a adição de overloading do construtor.

--- MadMotors/src/Models/Usuario.php
+++ MadMotors/src/Models/Usuario.php
@@ -2,7 +2,7 @@
 
     include('IModelo.php');
     include('Endereco.php');
-    include ('Anuncio.php');
+    include('Anuncio.php');
     include('MySQL.php');
 
     class Usuario implements IModelo
@@ -20,49 +20,53 @@
         public $telefone1;
         public $telefone2;
         public $idEndereco;
+        
         private $mySQL;
 
         public function __construct()
         {
-            
-        }
-        
-        /*public function __construct( $nome, $sexo, $dtNascimento, $rg, $nacionalidade, $naturalidade, $email, $telefone1, $telefone2, $idEndereco)
-        {
-			$mySQL = new MySQL;
-			$this -> nome  = $nome;
-			$this -> sexo = $sexo;
-			$this -> dtNascimento = $dtNascimento;
-			$this -> rg = $rg;
-			$this -> nacionalidade = $nacionalidade;
-			$this -> naturalidade = $naturalidade;
-			$this -> email = $email;
-			$this -> telefone1 = $telefone1;
-			$this -> telefone2 = $telefone2;
-			$this -> idEndereco = $idEndereco;
-		}*/
+            $count = func_num_args();
+            if($count != 11)
+            {
+				echo "Número de parâmetros Incorreto";
+			}
+			else
+			{
+				list($nome, $sexo, $dtNascimento, $rg, $nacionalidade, $naturalidade, $email, $telefone1, $telefone2, $idEndereco) = func_get_args();
+				$this -> nome  = $nome;
+				$this -> sexo = $sexo;
+				$this -> dtNascimento = $dtNascimento;
+				$this -> rg = $rg;
+				$this -> nacionalidade = $nacionalidade;
+				$this -> naturalidade = $naturalidade;
+				$this -> email = $email;
+				$this -> telefone1 = $telefone1;
+				$this -> telefone2 = $telefone2;
+				$this -> idEndereco = $idEndereco;
+			}
+        }       
 
-        public static function insert()
+        public function insert()
         {
 			$mySQL = new MySQL;
             $mySQL->connect();
             $insereUsuario = $mySQL->executeQuery("INSERT INTO 'Usuario'('nome','sexo','dtNascimento','rg','nacionalidade','naturalidade','email','foto','telefone1','telefone2','id_Endereco') VALUES ('" . $nome . "','" . $sexo . "'," . $dtNascimento . ",'" . $rg . "','" . $nacionalidade . "','" . $naturalidade . "','" . $email . "',1," . $telefone1 . "," . $telefone2 . "," . $idEndereco . ");");
         }
-        public static function update()
+        public function update()
         {
 			$mySQL = new MySQL;
             $mySQL->connect();
 			$updateModelo = $mySQL->executeQuery("UPDATE Usuario SET  nome =  '".$nome."', sexo = '".$sexo."', dtNascimento = ".$dtNascimento.", rg = '".$rg."', nacionalidade = '".$nacionalidade."', naturalidade = '".$naturalidade."', email = '".$email."', foto = ".$foto.", telefone1 = ".$telefone1.", telefone2 = ".$telefone2.", id_Endereco = ".$id_Endereco." WHERE id = ".$id."");
 		}
 
-        public static function delete()
+        public function delete()
         {
 			$mySQL = new MySQL;
             $mySQL->connect();
 			$deleteModelo = $mySQL->executeQuery("DELETE FROM Usuario WHERE id = ".$id."");
 		}
 
-        public static function select()
+        public function select()
         {			
 			$selectUsuario = "SELECT * FROM Usuario ";
 			
